63dcf195301edb3dfe0aa56694ceda588648d5ac
shikhar004dot-com <shikharsrivastava816@gmail.com>
2026-04-18 23:10:21 +0530

    edited code

--- Searching_Algos/LinearSearch.java
+++ Searching_Algos/LinearSearch.java
@@ -1,13 +1,13 @@
 class LinearSearch{
     public static void main(String[] args) {
         int arr[]= {2,3,1,4,5,6};
-        System.out.println(LinearSearch(arr, 4));
+        System.out.println(linearSearch(arr, 4));
 
     }
-    public static int LinearSearch(int arr[],int key){
+    public static int linearSearch(int arr[],int key){
         for(int i=0;i<arr.length;i++){  // O(n)
             if(arr[i]==key){
-                return key;
+                return i;
             }
         }
         return -1;
